362bab4c97e83814b4726bfeb608303ac9f799aa
marlowe <marlowepdx@gmail.com>
2022-09-10 17:39:51 -0700

    reated gallery/index.js and included it in the app componen t

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import About from './components/About';
 import Nav from './components/Nav';
+import Gallery from './components/Gallery';
 
 function App() {
   return (
     <div>
       <Nav></Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
 
